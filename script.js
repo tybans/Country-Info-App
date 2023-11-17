8049f6433d560ad9c3ff8ff439d42cdf6135aa98
Taiyab Ansari <taiyab0523@gmail.com>
2023-11-17 09:45:47 +0530

    Created Country Information App

---
+++ script.js
@@ -0,0 +1,70 @@
+let searchBtn = document.querySelector(".btn");
+let inputBar = document.getElementById("country-input");
+
+searchBtn.addEventListener("click", () =>{
+    let countryName = inputBar.value;
+    let url = `https://restcountries.com/v3.1/name/${countryName}?all`;
+    console.log(url);
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) =>{
+        
+
+        result.innerHTML = `
+            <img src="${data[0].flags.svg}"
+            class = "flag-img">
+
+            <h2>${data[0].name.common}</h2>
+
+            <div class = "wrapper">
+                <div class = "data-wrapper">
+                    <h4>Capital: </h4>
+                    <span>${data[0].capital[0]}</span>
+                </div>
+            </div>
+            <div class = "wrapper">
+                <div class = "data-wrapper">
+                    <h4>Continent: </h4>
+                    <span>${data[0].continents[0]}</span>
+                </div>
+            </div>
+            <div class = "wrapper">
+                <div class = "data-wrapper">
+                    <h4>Population: </h4>
+                    <span>${data[0].population}</span>
+                </div>
+            </div>
+            <div class = "wrapper">
+                <div class = "data-wrapper">
+                    <h4>Currency: </h4>
+                    <span>${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)[0]}</span>
+                </div>
+            </div>
+            <div class = "wrapper">
+                <div class = "data-wrapper">
+                    <h4>Common Language: </h4>
+                    <span>${Object.values(data[0].languages).toString().split(",").join(", ")}</span>
+                </div>
+            </div>
+            
+        `
+    })
+    .catch(() =>{
+        if(countryName.length == 0){
+            result.innerHTML= `<h3>Just enter Any Country name!</h3>`;
+        }
+        else{
+            result.innerHTML = `<h3>Couldn't get the result, please enter a valid Country Name!`
+        }
+    })
+})
+
+// async function fetchCountry (){
+//     let response = await fetch (" https://restcountries.com/v3.1/all ");
+//     let data = await response.json();
+//     // console.log(data[0]);
+
+//     let mainBody = document.querySelector(".result")
+//     console.log(object.values(data[0].name).toString().split(",").join(", "));
+// }
+// fetchCountry();
